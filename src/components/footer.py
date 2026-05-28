36c45fd6aa5786e39684a3fb4a97917acf7a989f
Saikat Das <sdsaikatdas2005@gmail.com>
2026-05-28 14:32:19 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def footer_home():
+
+    st.markdown("""
+        <div style="
+            margin-top:1rem;
+            padding-bottom:1rem;
+            text-align:center;
+            color:white;
+            font-size:15px;
+            font-weight:400;
+            line-height:1.8;
+        ">
+            SnapClass • AI Attendance System <br>
+            Built with Streamlit & Python
+        </div>
+    """, unsafe_allow_html=True)
