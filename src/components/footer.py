6c46517dcc7eed79a890e93d33352ab6e3511b0e
Saikat Das <sdsaikatdas2005@gmail.com>
2026-06-26 14:54:18 +0530

    Finishing Touches

--- src/components/footer.py
+++ src/components/footer.py
@@ -17,3 +17,21 @@ def footer_home():
             Built with Streamlit & Python
         </div>
     """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+
+    st.markdown("""
+        <div style="
+            margin-top:1rem;
+            padding-bottom:1rem;
+            text-align:center;
+            color:black;
+            font-size:15px;
+            font-weight:400;
+            line-height:1.8;
+        ">
+            SnapClass • AI Attendance System <br>
+            Built with Streamlit & Python
+        </div>
+    """, unsafe_allow_html=True)
